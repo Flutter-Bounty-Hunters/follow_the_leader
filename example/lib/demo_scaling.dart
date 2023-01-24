423cd7bbd8f701507f31366e1d12f331122a2ae9
Matt Carroll <matthew-carroll@users.noreply.github.com>
2023-01-23 22:32:43 -0800

    Fix analysis error

--- example/lib/demo_scaling.dart
+++ example/lib/demo_scaling.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:follow_the_leader/follow_the_leader.dart';
 import 'package:overlord/follow_the_leader.dart';
