9601e334c7f4a8bbcd14d581518d24cea869f007
Matt Carroll <matthew-carroll@users.noreply.github.com>
2023-04-24 17:45:54 -0700

    Adjusted fade boundary to only fade when all of the follower is out of bounds (Resolves #30) (#33)

--- test_goldens/smoke_golden_test.dart
+++ test_goldens/smoke_golden_test.dart
@@ -15,32 +15,54 @@ void main() {
       );
     });
 
-    testWidgets("restrict static positions to boundary", (widgetTester) async {
+    testWidgets("restrict to boundary", (widgetTester) async {
       // Variants tests four corners of alignment.
       await _pumpBoundedFollowerScenario(
         widgetTester,
-        leaderAlignment: _FourAlignmentVariant.currentAlignment!,
+        leaderAlignment: _CornerAlignmentBeyondBoundaryVariant.currentAlignment!,
+        constrainFollowerToBoundary: true,
       );
       await expectLater(
         find.byType(MaterialApp),
         matchesGoldenFile(
-            "goldens/follower_static_initial-placement_out-of-bounds_${_FourAlignmentVariant.currentDescription!}.png"),
+            "goldens/follower_restricts-to-boundary_${_CornerAlignmentBeyondBoundaryVariant.currentDescription!}.png"),
       );
-    }, variant: const _FourAlignmentVariant());
+    }, variant: const _CornerAlignmentBeyondBoundaryVariant());
 
-    testWidgets("don't render static positions outside of boundary with a fade policy", (widgetTester) async {
+    testWidgets("fade out when they are entirely outside of boundary with a fade policy", (widgetTester) async {
       // Variants tests four corners of alignment.
       await _pumpBoundedFollowerScenario(
         widgetTester,
-        leaderAlignment: _FourAlignmentVariant.currentAlignment!,
+        leaderAlignment: _CornerAlignmentBeyondBoundaryVariant.currentAlignment!,
         fadeOutBeyondBoundary: true,
       );
       await expectLater(
         find.byType(MaterialApp),
         matchesGoldenFile(
-            "goldens/follower_static_initial-placement_out-of-bounds-no-render_${_FourAlignmentVariant.currentDescription!}.png"),
+            "goldens/follower_fade-when-out-of-bounds_${_CornerAlignmentBeyondBoundaryVariant.currentDescription!}.png"),
       );
-    }, variant: const _FourAlignmentVariant());
+    }, variant: const _CornerAlignmentBeyondBoundaryVariant());
+
+    testWidgets("don't fade when they partially overlap boundary with a fade policy", (widgetTester) async {
+      // Variants tests four corners of alignment.
+      await _pumpBoundedFollowerScenario(
+        widgetTester,
+        leaderAlignment: _CornerAlignmentPartialOverlapVariant.currentAlignment!,
+        fadeOutBeyondBoundary: true,
+      );
+
+      // Wait for the follower to animate to opaque. The very first frame probably didn't
+      // have the Leader connected yet. The second frame made the connection. Which means the
+      // Follower starts invisible, then decides to become visible, and this requires an
+      // animation to complete.
+      await widgetTester.pumpAndSettle();
+
+      await expectLater(
+        find.byType(MaterialApp),
+        matchesGoldenFile(
+            "goldens/follower_static_no-fade-when-partial-overlap_${_CornerAlignmentPartialOverlapVariant.currentDescription!}.png"),
+      );
+    }, variant: const _CornerAlignmentPartialOverlapVariant());
   });
 }
 
@@ -55,6 +77,7 @@ Future<void> _pumpBoundedFollowerScenario(
   required leaderAlignment,
   LeaderLink? leaderLink,
   bool fadeOutBeyondBoundary = false,
+  bool constrainFollowerToBoundary = false,
 }) async {
   widgetTester.binding.window
     ..devicePixelRatioTestValue = 1.0
@@ -96,7 +119,7 @@ Future<void> _pumpBoundedFollowerScenario(
           child: Follower.withOffset(
             link: link,
             offset: const Offset(0, -50),
-            boundary: widgetBoundary,
+            boundary: constrainFollowerToBoundary ? widgetBoundary : null,
             child: Container(
               width: 50,
               height: 50,
@@ -112,14 +135,14 @@ Future<void> _pumpBoundedFollowerScenario(
   await widgetTester.pump();
 }
 
-class _FourAlignmentVariant extends TestVariant<Alignment> {
+class _CornerAlignmentBeyondBoundaryVariant extends TestVariant<Alignment> {
   static Alignment? get currentAlignment => _currentAlignment;
   static Alignment? _currentAlignment;
 
   static String? get currentDescription => _currentDescription;
   static String? _currentDescription;
 
-  const _FourAlignmentVariant();
+  const _CornerAlignmentBeyondBoundaryVariant();
 
   @override
   Iterable<Alignment> get values => const [
@@ -127,6 +150,46 @@ class _FourAlignmentVariant extends TestVariant<Alignment> {
         Alignment(0.9, -0.9),
         Alignment(0.9, 0.9),
         Alignment(-0.9, 0.9),
+      ];
+
+  @override
+  String describeValue(Alignment value) {
+    final horizontal = value.x < 0.0 ? "left" : "right";
+    final vertical = value.y < 0.0 ? "top" : "bottom";
+    return "$vertical-$horizontal";
+  }
+
+  @override
+  Future<Object?> setUp(Alignment value) async {
+    _currentAlignment = value;
+    _currentDescription = describeValue(value);
+    return null;
+  }
+
+  @override
+  Future<void> tearDown(Alignment value, covariant Object? memento) async {
+    _currentAlignment = null;
+    _currentDescription = null;
+  }
+}
+
+/// Positions leaders so that they sit partially inside, and outside of the
+/// boundary region, at every corner.
+class _CornerAlignmentPartialOverlapVariant extends TestVariant<Alignment> {
+  static Alignment? get currentAlignment => _currentAlignment;
+  static Alignment? _currentAlignment;
+
+  static String? get currentDescription => _currentDescription;
+  static String? _currentDescription;
+
+  const _CornerAlignmentPartialOverlapVariant();
+
+  @override
+  Iterable<Alignment> get values => const [
+        Alignment(-0.6, -0.6),
+        Alignment(0.6, -0.6),
+        Alignment(0.6, 0.6),
+        Alignment(-0.6, 0.6),
       ];
 
   @override
